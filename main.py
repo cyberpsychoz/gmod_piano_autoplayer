cbf50cc6bc2ee923526bd2f3b9e17b34b932edac
Крыжовник <vasyl.lebedev@yandex.ru>
2023-02-17 15:01:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,53 @@ import mido
 from pynput.keyboard import Controller, Key
 import time
 
-# OОткрывает MIDI файл
+# Открывает MIDI файл
 midi_file = mido.MidiFile('example.mid')
 
-# Определияет контроллер клавиатуры
+# Определяет контроллер клавиатуры
 keyboard = Controller()
 
 # Определяет отображение MIDI-нот на клавиши клавиатуры
 key_map = {
-    60: Key.space,    # C4 -> пробел
-    62: Key.right,    # D4 -> вправо
-    64: Key.left,     # E4 -> влево
-    65: Key.down,     # F4 -> вниз
-    67: Key.up,       # G4 -> вверх
+    'C4': 1,
+    'C#4':1,
+    'D4': 2,
+    'D#4': 2,
+    'E4': 3,
+    'F4': 4,
+    'F#4': 4,
+    'G4': 5,
+    'G#4': 5,
+    'A4': 6,
+    'A#4': 6,
+    'B4': 7,
+    'C5': 8,
 }
 
 # Повторяет цикл над каждым сообщением в MIDI-файле
 for msg in midi_file.play():
     # Если сообщение является примечанием к сообщению и скорость больше 0
-    if msg.type == 'note_on' and msg.velocity > 0:
-        # Проверка заметки на карте ключей
-        if msg.note in key_map:
-            # Симуляция нажатий клавиши
-            keyboard.press(key_map[msg.note])
+    if msg.type == 'note_on' and msg.note in key_map and msg.velocity > 0:
+        # Определение имени ноты и клавиши
+        note_name = mido.midi2str(msg.note)
+        key = key_map[msg.note]
+        print('key_map')
+        # Если нота является знаком #, удерживаем клавишу Shift
+        if '#' in note_name:
+            keyboard.press(Key.shift)
+        # Симуляция нажатий клавиши
+        keyboard.press(key)
     # Если сообщение является сообщением об отключении записи или скорость равна 0
-    elif msg.type == 'note_off' or msg.velocity == 0:
+    elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
         # Проверка заметки на карте ключей
         if msg.note in key_map:
+            # Определение имени ноты и клавиши
+            note_name = mido.midi2str(msg.note)
+            key = key_map[msg.note]
+            # Если нота является знаком #, отпустите клавишу Shift
+            #if '#' in note_name:
+            #    keyboard.release(Key.shift)
             # Симуляция отпускания клавиши (типо уже нажата)
-            keyboard.release(key_map[msg.note])
+            keyboard.release(key)
     # Задержка
     time.sleep(msg.time)
